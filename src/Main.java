12213333314e1d7b1f2b36a13a37f8915d715139
Tragheit <tragheit@tutorials.com>
2017-12-10 01:54:18 +0100

    1.0 Calculator for Integers

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("userInterface.fxml"));
-        primaryStage.setTitle("Hello World");
+
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculator");
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
